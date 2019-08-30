9be270ffc80409114092cc808d7fe97e73653f08
MCS <mcs@MCSs-MacBook-Pro.local>
2019-08-29 23:58:04 -0400

    save

--- MyCodingChallengePlayground.playground/Contents.swift
+++ MyCodingChallengePlayground.playground/Contents.swift
@@ -2,22 +2,44 @@ import UIKit
 
 var array: [[Int]] = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
 
-func rotateArray(array: [[Int]]) -> [[Int]] {
+func rotateArray(array: [[Int]], numberOfRotations: Int) -> [[Int]] {
 
     var rotateArray = array
+    var reRotateArray = array
     
-    for i in 0...array.count-1 {
-        for j in 0...array[i].count - 1 {
-            
-            if i == 0 && (j == 0 || (j != 0 && j != array[i].count - 1)){
-                rotateArray[i][j] = array[i][j+1]
-            } else if i == array.count - 1 && (j == array[i].count - 1 || (j != 0 && j != array[i].count - 1)){
-                rotateArray[i][j] = array[i][j-1]
-            } else if (i == 0 || (i != 0 && i != array.count - 1)) && j == array[i].count - 1 {
-                rotateArray[i][j] = array[i+1][j]
-            } else if (i == array.count - 1 || (i != 0 && i != array.count - 1)) && j == 0 {
-                rotateArray[i][j] = array[i-1][j]
+    if numberOfRotations == 1 {
+        for i in 0...array.count-1 {
+            for j in 0...array[i].count - 1 {
+                
+                if i == 0 && (j == 0 || (j != 0 && j != array[i].count - 1)){
+                    rotateArray[i][j] = array[i][j+1]
+                } else if i == array.count - 1 && (j == array[i].count - 1 || (j != 0 && j != array[i].count - 1)){
+                    rotateArray[i][j] = array[i][j-1]
+                } else if (i == 0 || (i != 0 && i != array.count - 1)) && j == array[i].count - 1 {
+                    rotateArray[i][j] = array[i+1][j]
+                } else if (i == array.count - 1 || (i != 0 && i != array.count - 1)) && j == 0 {
+                    rotateArray[i][j] = array[i-1][j]
+                }
             }
+        }
+    } else if numberOfRotations > 1 {
+    
+        for k in 0...numberOfRotations - 1 {
+            for i in 0...reRotateArray.count-1 {
+                for j in 0...reRotateArray[i].count - 1 {
+                    
+                    if i == 0 && (j == 0 || (j != 0 && j != reRotateArray[i].count - 1)){
+                        rotateArray[i][j] = reRotateArray[i][j+1]
+                    } else if i == reRotateArray.count - 1 && (j == reRotateArray[i].count - 1 || (j != 0 && j != reRotateArray[i].count - 1)){
+                        rotateArray[i][j] = reRotateArray[i][j-1]
+                    } else if (i == 0 || (i != 0 && i != reRotateArray.count - 1)) && j == reRotateArray[i].count - 1 {
+                        rotateArray[i][j] = reRotateArray[i+1][j]
+                    } else if (i == reRotateArray.count - 1 || (i != 0 && i != reRotateArray.count - 1)) && j == 0 {
+                        rotateArray[i][j] = reRotateArray[i-1][j]
+                    }
+                }
+            }
+            reRotateArray = rotateArray
         }
     }
     print(array[0])
@@ -32,4 +54,4 @@ func rotateArray(array: [[Int]]) -> [[Int]] {
     return rotateArray
 }
 
-rotateArray(array: array)
+rotateArray(array: array, numberOfRotations: 12)
